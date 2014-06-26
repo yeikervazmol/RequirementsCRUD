7c9b00c7541f1cae9e2be66b51fb3441ac7d96f9
yvazquez <yeikervazmol@gmail.com>
2014-06-26 11:53:52 -0430

    + Se comienza a utiliza findOne. + Se regresa json en caso de que el usuario no este autenticado pero aun hay problemas para generar la vista cuando esto ocurre.

--- routes/controlador.js
+++ routes/controlador.js
@@ -1,32 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Aqui se autentica al usuario de forma sencilla y se obtienen sus proyectos.
 router.post('/login', function(req, res) {
 	var username 			= req.body.usuario;
 	var password 			= req.body.clave;
 	var db 					= req.db;
 	var userCollection 		= db.get('usuario');
 	var projectCollection 	= db.get('proyecto');
-	userCollection.find({nombre: username, clave: password}, 'nombre', function(e,users){
 
-		projectCollection.find({usuario: username}, {}, function(e,projects){
-		    res.render('test0', 
-			    {
-			        usuarios: users,
-			        proyectos: projects
-			    }
-			);
-		});
+	userCollection.findOne(
+		{nombre: username, clave: password}, 
+		function(e,user){
+
+		if(user == null){
+			// Caso en el que el usuario no ha sido autenticado.
+
+			return res.json(
+			//return res.render('test0',
+				    {
+				        usuario: "Usuario no autorizado"
+				    }
+				);
+		} else {
+
+			projectCollection.find(
+				{usuario: username}, 
+				function(e,projects){
+
+			    return res.render('test0', 
+			    //res.json(
+				    {
+				        usuario: user.nombre,
+				        proyectos: projects
+				    }
+				);
+			});
+		}
 	});
 });
 
+// Se obtienen los requerimientos asociados a un usuario y proyecto dado.
 router.get('/requirements', function(req, res) {
 	var project 				= req.param('proyecto');
 	var username 				= req.param('usuario');
 	var db 						= req.db;
 	var requirementCollection 	= db.get('requerimiento');
-	requirementCollection.find({usuario: username, proyecto: project}, 'nombre', function(e,requirements){
+
+	requirementCollection.find(
+		{usuario: username, proyecto: project}, 
+		'nombre', 
+		function(e,requirements){
 
 	    res.render('test1', 
 		    {
@@ -37,22 +61,30 @@ router.get('/requirements', function(req, res) {
 	});
 });
 
+// Se obtienen las tareas asociadas a un usuario, proyecto y requerimiento 
+// dado.
 router.get('/tasks', function(req, res) {
 	var project 				= req.param('proyecto');
 	var username 				= req.param('usuario');
-	var requirement 			= req.param('requerimiento');
+	var requirementName 		= req.param('requerimiento');
 	var db 						= req.db;
 	var requirementCollection 	= db.get('requerimiento');
 	var taskCollection 			= db.get('tarea');
-	requirementCollection.find({nombre: requirement, usuario: username, proyecto: project}, {}, function(e,requirements){
-		taskCollection.find({usuario: username, proyecto: project, requerimiento: requirement}, {}, function(e,tasks){
+
+	requirementCollection.findOne(
+		{nombre: requirementName, usuario: username, proyecto: project}, 
+		function(e,requirement){
+
+		taskCollection.find(
+			{usuario: username, proyecto: project, requerimiento: requirementName}, 
+			function(e,tasks){
 
 		    res.render('test2', 
 			    {
 			        proyecto 		: project,
-			        requerimiento 	: requirement,
-			        descripcion 	: requirements[0].descripcion,
-			        categoria 		: requirements[0].clasificacion,
+			        requerimiento 	: requirementName,
+			        descripcion 	: requirement.descripcion,
+			        categoria 		: requirement.clasificacion,
 			        tareas 			: tasks
 			    }
 			);
